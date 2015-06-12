fabc0b826b70f96e91e8272086919a482f1b1ae7
Steven Maude <git@stevenmaude.co.uk>
2015-06-12 15:21:52 +0100

    Change SQL examples
    SQL backend only allows SELECT right now. Removing SELECT from the
    query input box is the simplest fix.
    
    An alternative is allowing SELECT still, but just removing this
    from the string that is sent to the backend. This has greater
    possibility for not covering all the possible cases, so rejected this
    idea for now.

--- app/static/sql_query_tool_code.js
+++ app/static/sql_query_tool_code.js
@@ -95,8 +95,8 @@ var use_default_query_if_needed = function() {
 var use_default_query = function() {
   table = Object.keys(meta.table)[0]
   cols = meta.table[table].columnNames
-  data = "select \n" +
-      "\t" + cols.slice(0, 3).join(",\n\t") + "\n" +
+  data =
+      cols.slice(0, 3).join(",") + "\n" +
       "from " + table + "\n"
   if (cols.length > 2) {
       data += "-- where " + cols[Math.min(cols.length, 2)] + " > \n" +
@@ -112,9 +112,9 @@ var use_group_query = function() {
   table = Object.keys(meta.table)[0]
   cols = meta.table[table].columnNames
   var col = Math.min(cols.length, 2)
-  data = "select \n" +
-      "\t" + cols[col] + ",\n" +
-      "\tcount(*) as c\n" +
+  data =
+      cols[col] + "," +
+      "count(*) as c\n" +
       "from " + table + "\n"
   data += "group by " + cols[col] + "\n"
   data += "order by c desc\n"
